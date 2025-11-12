19cbea3437cd0a047c12eafa810e29ae5c1b2d67
bappaditya92 <bappadityasinha.92@gmail.com>
2025-11-12 23:21:19 +0530

    add create user api

--- crud/controllers/user.js
+++ crud/controllers/user.js
@@ -1,12 +1,30 @@
+import { use } from "react";
+import { User } from "../models/user";
 export const register = async ()=>{
     try {
-        const {fullname, email, password} = request.body;
-        if(!fullname || email || !password){
+        const {fullName, email, password} = request.body;
+        if(!fullName || email || !password){
             return res.status(403).json({
                 success: false,
                 message: "All field are required."
             })
         }
+        //find the user already exist or not
+        const user = await User.findOne(email);
+        if(user){
+            return res.status(403).json({
+                success: false,
+                message: "This email id is already exist"
+            })
+        }
+        await User.create({
+            fullName,email,password
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User created sucessfully"
+        })
     } catch (error) {
         
     }
